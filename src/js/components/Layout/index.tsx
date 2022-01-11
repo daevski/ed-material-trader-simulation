5b5fe67e16a1030df0d2bedf9d016d2bf9441f60
David McKee <daevski@gmail.com>
2022-01-11 11:56:07 -0500

    adding eslint and prettier

--- src/js/components/Layout/index.tsx
+++ src/js/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-import React from 'https://cdn.skypack.dev/react';
+import * as React from 'https://cdn.skypack.dev/react';
 
 import PageHeader from './PageHeader';
 import ConversionTable from './ConversionTable/index';
@@ -7,35 +7,40 @@ import ConversionTableControls from './ConversionTableControls';
 import UserDataSection from './UserDataSection';
 import { getFile } from './layoutFunctions';
 
-export default class Layout extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            userData: null
-        }
-    }
+type LayoutProps = {
+    materialData: any;
+};
 
-    getFile = () => getFile.bind(this)
+type LayoutState = {
+    userData: any;
+};
+
+export default class Layout extends React.Component<LayoutState, LayoutProps> {
+    state: LayoutState = { userData: null };
+    props: LayoutProps = { materialData: null };
+    // FIXME: This binding is not functional. Need to review class method binding syntax/approach
+    getFile = (e: Event) => getFile.bind(this);
     render() {
-        var allMaterialData = this.props.materialData
+        var allMaterialData = this.props.materialData;
         return (
             <div>
-                {/* Hello, {this.state.username}! */}
                 <PageHeader />
                 <UserDataSection getFile={this.getFile} />
                 <div id="conversion-app">
                     <hr />
                     <ConversionTableControls materialData={allMaterialData} />
-                    {
-                        Object.entries(allMaterialData['Materials'])
-                            .map(([_, typesArray]) =>
-                                Object.entries(typesArray)
-                                    .map(([materialType, _]) =>
-                                        <ConversionTable userData={this.state.userData} materialData={allMaterialData} materialType={materialType} active={materialType == "Raw" ? true : false} />
-                                    ))
-                    }
+                    {Object.entries(allMaterialData['Materials']).map(([_, typesArray]) =>
+                        Object.entries(typesArray).map(([materialType, _]) => (
+                            <ConversionTable
+                                userData={this.state.userData}
+                                materialData={allMaterialData}
+                                materialType={materialType}
+                                active={materialType == 'Raw' ? true : false}
+                            />
+                        )),
+                    )}
                 </div>
-            </div >
+            </div>
         );
     }
 }
